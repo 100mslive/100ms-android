2d592ccf45ac640d90d947fc220114348e63795c
Aditya Kumar <k.aditya00@gmail.com>
2021-01-13 19:29:22 +0530

    feat(layout): Chat, Meeting & Home

--- app/src/main/java/live/hms/android100ms/MainActivity.kt
+++ app/src/main/java/live/hms/android100ms/MainActivity.kt
@@ -1,11 +1,9 @@
 package live.hms.android100ms
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,11 +11,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
-
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
