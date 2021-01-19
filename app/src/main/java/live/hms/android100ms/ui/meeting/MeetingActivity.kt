b82e02195710152534afc2b9a79e6a09bfe836a4
Aditya Kumar <k.aditya00@gmail.com>
2021-01-19 08:52:14 +0530

    fix: show icons in meeting menu

--- app/src/main/java/live/hms/android100ms/ui/meeting/MeetingActivity.kt
+++ app/src/main/java/live/hms/android100ms/ui/meeting/MeetingActivity.kt
@@ -1,8 +1,10 @@
 package live.hms.android100ms.ui.meeting
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.view.menu.MenuBuilder
 import live.hms.android100ms.R
 import live.hms.android100ms.databinding.ActivityMeetingBinding
 import live.hms.android100ms.util.viewLifecycle
@@ -18,8 +20,14 @@ class MeetingActivity : AppCompatActivity() {
     supportActionBar?.setDisplayShowTitleEnabled(false)
   }
 
+  @SuppressLint("RestrictedApi")
   override fun onCreateOptionsMenu(menu: Menu?): Boolean {
     menuInflater.inflate(R.menu.menu_meeting, menu)
+
+    if (menu is MenuBuilder) {
+      menu.setOptionalIconsVisible(true)
+    }
+
     return true
   }
 }
