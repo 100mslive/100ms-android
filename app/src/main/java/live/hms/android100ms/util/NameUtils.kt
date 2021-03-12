88b06eae0a90e73ac7946ed7d06af23a78c2e34c
Aditya Kumar <k.aditya00@gmail.com>
2021-03-12 10:52:49 +0530

    fix(name-utils): keep atmost 2 characters

--- app/src/main/java/live/hms/android100ms/util/NameUtils.kt
+++ app/src/main/java/live/hms/android100ms/util/NameUtils.kt
@@ -8,7 +8,7 @@ object NameUtils {
     } else if (words.size == 1) {
       return words[0].substring(0, words[0].length.coerceAtMost(2))
     } else {
-      return "${words[0][0]}${words[1][1]}"
+      return "${words[0][0]}${words[1][0]}"
     }
   }
 }
