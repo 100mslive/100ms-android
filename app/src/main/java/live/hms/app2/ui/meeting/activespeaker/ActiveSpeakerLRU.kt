5331bfc5cc3ed8fdaf137b16e10049c7fd2b241e
Aditya Kumar <k.aditya00@gmail.com>
2021-05-27 16:01:21 +0530

    docs(active-speaker): method & fields docstrings

--- app/src/main/java/live/hms/app2/ui/meeting/activespeaker/ActiveSpeakerLRU.kt
+++ app/src/main/java/live/hms/app2/ui/meeting/activespeaker/ActiveSpeakerLRU.kt
@@ -1,22 +1,50 @@
 package live.hms.app2.ui.meeting.activespeaker
 
 import android.util.Log
+import java.util.*
 
-class ActiveSpeakerLRU<T>(val capacity: Int) {
+/**
+ * Active Speaker LRU - Aims to fetch the most active speakers
+ * in the order which requires minimum swaps from the previous
+ * state -- ensuring that the UI is not updated too frequently.
+ *
+ * @param capacity - Maximum number of items in the [queue]
+ */
+class ActiveSpeakerLRU<T>(
+  val capacity: Int
+) {
   companion object {
     private const val TAG = "ActiveSpeakerLRU"
   }
 
+  /**
+   * Timer used to assign [Item.timestamp] values to each [Item]
+   */
   private var timer = 0L
 
+  /**
+   * Wrapper over [T], adding [timestamp] to allow comparison
+   * between two [Item]'s
+   */
   private data class Item<T>(
     val value: T,
     var timestamp: Long
   )
 
-  private val queue = ArrayList<Item<T>>()
+  /**
+   * Thread-safe queue which stores each [Item] each representing the
+   * most active [T].
+   *
+   * When a new items needs to be pushed we follow algorithm defined in
+   * [update] method.
+   */
+  private val queue = Collections.synchronizedList(ArrayList<Item<T>>())
+
+  /**
+   * Gets the current number of elements in the queue
+   */
   val size: Int
-    get() = queue.size
+    @Synchronized get() = queue.size
 
   private fun getItemIndex(value: T): Int {
     for ((i, item) in queue.withIndex()) {
@@ -45,6 +73,10 @@ class ActiveSpeakerLRU<T>(val capacity: Int) {
     return idx
   }
 
+  /**
+   * Resets the [Item.timestamp] values starting with 0
+   * This prevents the overflow of [timer] values
+   */
   private fun normalize() {
     Log.d(TAG, "normalize: START $queue")
     timer = 0
@@ -54,9 +86,23 @@ class ActiveSpeakerLRU<T>(val capacity: Int) {
     Log.d(TAG, "normalize: DONE $queue")
   }
 
+  /**
+   *
+   * For each current item in [items]:
+   *  1. Check if current value exists already in the [queue] - update
+   *  the timestamp to [timer] + 1
+   *  2. Else if [size] < [capacity] - push the [Item] simply
+   *  into the [queue]
+   *  3. Else, find and replace the item with minimum [Item.timestamp]
+   *  in the queue with the current item
+   *
+   * @param items - List of items to be pushed / updated in the [queue]
+   *  The item are assumed to be sorted from loud -> silent
+   */
+  @Synchronized
   fun update(items: List<T>) {
     var index = 0
-    for (item in items.take(capacity)) {
+    for (item in items.take(capacity).asReversed()) {
       val position = getItemIndex(item)
       if (position == -1) {
         // New entry
@@ -82,6 +128,10 @@ class ActiveSpeakerLRU<T>(val capacity: Int) {
     }
   }
 
+  /**
+   * Removes any item from the [queue]
+   */
+  @Synchronized
   fun remove(items: List<T>) {
     for (item in items) {
       val position = getItemIndex(item)
@@ -91,5 +141,10 @@ class ActiveSpeakerLRU<T>(val capacity: Int) {
     }
   }
 
+  /**
+   * Simply get all the elements in the [queue] maintaining
+   * the order
+   */
+  @Synchronized
   fun getItemsInOrder() = queue.map { it.value }
 }
