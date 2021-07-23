730f11a90ae03bb239cd9102c51896311081cf83
pratim <pratim@100ms.live>
2021-07-23 17:28:29 +0530

    removing unused builder option

--- app/src/main/java/live/hms/app2/ui/meeting/MeetingViewModel.kt
+++ app/src/main/java/live/hms/app2/ui/meeting/MeetingViewModel.kt
@@ -121,7 +121,6 @@ class MeetingViewModel(
 
   private val hmsSDK = HMSSDK
     .Builder(application)
-    .setSubscribeDegradation(settings.enableSubscribeDegradation)
     .build()
 
   val peers: Array<HMSPeer>
