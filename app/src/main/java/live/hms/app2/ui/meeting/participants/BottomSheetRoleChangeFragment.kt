8fba2ebdbd8660584d9fd290af758a6e859a777b
Aniket Kadam <aniket.kadam@100ms.live>
2021-07-28 17:46:09 +0530

    Added bindings and the vm

--- app/src/main/java/live/hms/app2/ui/meeting/participants/BottomSheetRoleChangeFragment.kt
+++ app/src/main/java/live/hms/app2/ui/meeting/participants/BottomSheetRoleChangeFragment.kt
@@ -4,16 +4,36 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import live.hms.app2.R
+import live.hms.app2.databinding.FragmentParticipantsBinding
+import live.hms.app2.databinding.LayoutFragmentBottomSheetChangeRoleBinding
+import live.hms.app2.ui.meeting.MeetingViewModel
+import live.hms.app2.util.viewLifecycle
 
 class BottomSheetRoleChangeFragment : BottomSheetDialogFragment() {
+    private val meetingViewModel: MeetingViewModel by activityViewModels()
+
+    private var binding by viewLifecycle<LayoutFragmentBottomSheetChangeRoleBinding>()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         super.onCreateView(inflater, container, savedInstanceState)
-        return inflater.inflate(R.layout.layout_fragment_bottom_sheet_change_role, container, false);
+        binding = LayoutFragmentBottomSheetChangeRoleBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.cancel.setOnClickListener { findNavController().popBackStack() }
     }
 }
