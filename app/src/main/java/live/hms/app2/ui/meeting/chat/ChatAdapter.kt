1c938ec70d1452cf936b8ad801f4be173b249c3a
Aniket Kadam <aniket.kadam@100ms.live>
2021-08-13 13:51:16 +0530

    Using recipient info to show "to" messages

--- app/src/main/java/live/hms/app2/ui/meeting/chat/ChatAdapter.kt
+++ app/src/main/java/live/hms/app2/ui/meeting/chat/ChatAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import live.hms.app2.databinding.ListItemChatBinding
+import live.hms.video.sdk.models.enums.HMSMessageRecipientType
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -18,11 +19,18 @@ class ChatAdapter(
     RecyclerView.ViewHolder(binding.root) {
 
     fun bind(message: ChatMessage) {
-      binding.name.text = message.senderName
+      binding.name.text = "${message.senderName}${getRecipientText(message)}"
       binding.message.text = message.message
       binding.blueBar.visibility = if (message.isSentByMe) View.VISIBLE else View.GONE
       binding.time.text = dateFormatter.format(message.time)
     }
+
+    private fun getRecipientText(message: ChatMessage): String =
+      when(message.recipient.recipientType) {
+        HMSMessageRecipientType.BROADCAST -> ""
+        HMSMessageRecipientType.PEER -> " (to ${message.recipient.recipientPeer?.name})"
+        HMSMessageRecipientType.ROLES -> " (to ${message.recipient.recipientRoles.firstOrNull()?.name})"
+      }
   }
 
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatMessageViewHolder {
