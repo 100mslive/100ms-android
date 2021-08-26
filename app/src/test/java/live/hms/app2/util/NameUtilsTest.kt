b1de721abdf8e624f67c46c0bc520cd2cea2a20a
Aniket Kadam <aniket.kadam@100ms.live>
2021-08-26 18:35:19 +0530

    Added tests for name utils

---
+++ app/src/test/java/live/hms/app2/util/NameUtilsTest.kt
@@ -0,0 +1,29 @@
+package live.hms.app2.util
+
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert
+import org.junit.Test
+
+class NameUtilsTest {
+
+    @Test
+    fun `handles usual double names`() {
+        MatcherAssert.assertThat(NameUtils.getInitials("Praveen Jaiswal"), equalTo("PJ"))
+    }
+
+    @Test
+    fun `handles single names`() {
+        MatcherAssert.assertThat(NameUtils.getInitials("Praveen"), equalTo("PR"))
+    }
+
+    @Test
+    fun `handles single character names`() {
+        MatcherAssert.assertThat(NameUtils.getInitials("P"), equalTo("P"))
+    }
+
+
+    @Test
+    fun `handles double spaced names`() {
+        MatcherAssert.assertThat(NameUtils.getInitials("Praveen  Jaiswal"), equalTo("PJ"))
+    }
+}
