fcf201840d396463101f8916c6c4ee318fbadc99
Aniket Kadam <aniket.kadam@100ms.live>
2021-09-06 09:57:36 +0530

    Correct the second check in the hero fragmnent for the video cast

--- app/src/main/java/live/hms/app2/ui/meeting/pinnedvideo/PinnedVideoFragment.kt
+++ app/src/main/java/live/hms/app2/ui/meeting/pinnedvideo/PinnedVideoFragment.kt
@@ -16,7 +16,6 @@ import live.hms.app2.ui.meeting.MeetingTrack
 import live.hms.app2.ui.meeting.MeetingViewModel
 import live.hms.app2.ui.meeting.MeetingViewModelFactory
 import live.hms.app2.util.*
-import live.hms.video.media.tracks.HMSVideoTrack
 import org.webrtc.RendererCommon
 
 class PinnedVideoFragment : Fragment() {
@@ -140,7 +139,7 @@ class PinnedVideoFragment : Fragment() {
         }
 
         SurfaceViewRendererUtil.bind(this, track).let { success ->
-          if (success) visibility = if ((track as HMSVideoTrack).isDegraded) View.INVISIBLE else View.VISIBLE
+          if (success) visibility = if (track.video?.isDegraded == true) View.INVISIBLE else View.VISIBLE
         }
       }
     }
