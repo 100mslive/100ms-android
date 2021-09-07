df1a15fdd228711c0936c0cfe27cddfe78d2b460
Aniket Kadam <aniket.kadam@100ms.live>
2021-09-07 13:28:01 +0530

    Using a new speaker cache that selectively updates.

--- app/src/main/java/live/hms/app2/ui/meeting/activespeaker/ActiveSpeakerFragment.kt
+++ app/src/main/java/live/hms/app2/ui/meeting/activespeaker/ActiveSpeakerFragment.kt
@@ -1,7 +1,6 @@
 package live.hms.app2.ui.meeting.activespeaker
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +8,7 @@ import live.hms.app2.databinding.FragmentActiveSpeakerBinding
 import live.hms.app2.ui.meeting.MeetingTrack
 import live.hms.app2.ui.meeting.commons.VideoGridBaseFragment
 import live.hms.app2.util.viewLifecycle
+import live.hms.video.utils.HMSLogger
 import org.webrtc.RendererCommon
 
 class ActiveSpeakerFragment : VideoGridBaseFragment() {
@@ -18,10 +18,15 @@ class ActiveSpeakerFragment : VideoGridBaseFragment() {
   }
 
   private data class LruItem(
-    val peerId: String
-  )
+    val peerId: String,
+    val peerName: String
+  ) {
+    override fun toString(): String {
+      return peerName
+    }
+  }
 
-  private val lru by lazy { ActiveSpeakerLRU<LruItem>(maxItems) }
+  private val lru by lazy { ActiveSpeakerCache<LruItem>(4) }
   private var binding by viewLifecycle<FragmentActiveSpeakerBinding>()
 
   private var screenShareTrack: MeetingTrack? = null
@@ -52,7 +57,7 @@ class ActiveSpeakerFragment : VideoGridBaseFragment() {
   }
 
   private fun update() {
-    val order = lru.getItemsInOrder()
+    val order = lru.getAllItems()
     val videos = Array(order.size) { idx ->
       meetingViewModel.findTrack { it.peer.peerID == order[idx].peerId && it.isScreen.not() }
     }
@@ -60,78 +65,79 @@ class ActiveSpeakerFragment : VideoGridBaseFragment() {
   }
 
   private fun initViewModels() {
-    lru.update(meetingViewModel.mapTracks { if (it.isScreen) null else LruItem(it.peer.peerID) })
-    update()
 
     meetingViewModel.tracks.observe(viewLifecycleOwner) { tracks ->
-      Log.v(TAG, "tracks update received 🎼 [size=${tracks.size}]")
-      synchronized(tracks) {
-        // Update lru just to keep it as much filled as possible
-
-        val required = lru.capacity // We'd always want enough to fill the lru
-        val all = tracks
-          .sortedByDescending {
-            if(it.audio == null || it.audio?.isMute == true || it.isScreen){
-              it.peer.name.hashCode() * -1 // Drop these ids really low.
-            }
-            else
-              it.peer.name.hashCode()
-          }.take(required)
-          .map {
-            LruItem(it.peer.peerID)
-          }
-        Log.i(TAG, "$all")
-
-        lru.update(all)
-
-        // Check for tracks not present
-        val order = lru.getItemsInOrder()
-        val toRemove = order.filter { item ->
-          tracks.find {
-            it.isScreen.not() && it.peer.peerID == item.peerId
-          } == null
-        }
-        lru.remove(toRemove)
-        update()
-
-        // Check if the currently shared screen-share track is removed
-        screenShareTrack?.let { screen ->
-          if (tracks.find { screen == it } == null) {
-            screenShareTrack?.let { unbindSurfaceView(binding.screenShare, it) }
-            screenShareTrack = null
-          }
-        }
-
-        // Check for screen share
-        if (screenShareTrack == null) tracks.find { it.isScreen }?.let { screen ->
-          screenShareTrack = screen
-          if (isFragmentVisible) {
-            bindSurfaceView(
-              binding.screenShare,
-              screen,
-              RendererCommon.ScalingType.SCALE_ASPECT_FIT
-            )
-          }
-          bindVideo(binding.screenShare, screen)
-          binding.screenShare.apply {
-            iconAudioOff.visibility = View.GONE
-            iconScreenShare.visibility = View.GONE
-            audioLevel.visibility = View.GONE
-          }
-          binding.screenShareContainer.visibility = View.VISIBLE
-        }
-
-        if (screenShareTrack == null && binding.screenShareContainer.visibility != View.GONE) {
-          binding.screenShareContainer.visibility = View.GONE
-        }
-      }
+      HMSLogger.v(TAG, "tracks update received 🎼 [size=${tracks.size}]")
+      trackUpdateLruTrigger(tracks)
+      updateScreenshareTracks(tracks)
     }
 
     meetingViewModel.speakers.observe(viewLifecycleOwner) { speakers ->
-      Log.v(TAG, "speakers update received 🎙 [size=${speakers.size}]")
-      lru.update(speakers.map { LruItem(it.peerId) })
+      HMSLogger.v(
+        TAG,
+        "speakers update received 🎙 [size=${speakers.size}, names=${speakers.map { it.peer?.name }}] "
+      )
+
+      lru.update(speakers.map { LruItem(it.peer!!.peerID, it.peer!!.name) }, true)
       update()
+      // Active speaker should be updated via, tracks AND actual active speakers.
       applySpeakerUpdates(speakers)
+    }
+  }
+
+  private fun trackUpdateLruTrigger(tracks: List<MeetingTrack>) {
+    synchronized(tracks) {
+      // Update lru just to keep it as much filled as possible
+
+      val required = lru.capacity // We'd always want enough to fill the lru
+      val all = tracks
+        .sortedByDescending {
+          if (it.audio == null || it.audio?.isMute == true || it.isScreen) {
+            it.peer.name.hashCode() * -1 // Drop these ids really low.
+          } else
+            it.peer.name.hashCode()
+        }.take(required)
+        .map {
+          LruItem(it.peer.peerID, it.peer.name)
+        }
+
+      lru.update(all, false)
+
+      update()
+    }
+  }
+
+  private fun updateScreenshareTracks(tracks: List<MeetingTrack>) {
+
+    // Check if the currently shared screen-share track is removed
+    screenShareTrack?.let { screen ->
+      if (tracks.find { screen == it } == null) {
+        screenShareTrack?.let { unbindSurfaceView(binding.screenShare, it) }
+        screenShareTrack = null
+      }
+    }
+
+    // Check for screen share
+    if (screenShareTrack == null) tracks.find { it.isScreen }?.let { screen ->
+      screenShareTrack = screen
+      if (isFragmentVisible) {
+        bindSurfaceView(
+          binding.screenShare,
+          screen,
+          RendererCommon.ScalingType.SCALE_ASPECT_FIT
+        )
+      }
+      bindVideo(binding.screenShare, screen)
+      binding.screenShare.apply {
+        iconAudioOff.visibility = View.GONE
+        iconScreenShare.visibility = View.GONE
+        audioLevel.visibility = View.GONE
+      }
+      binding.screenShareContainer.visibility = View.VISIBLE
+    }
+
+    if (screenShareTrack == null && binding.screenShareContainer.visibility != View.GONE) {
+      binding.screenShareContainer.visibility = View.GONE
     }
   }
 }
