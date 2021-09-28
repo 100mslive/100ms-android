127351191204d80ed32f4dcbeb698062d0a182f9
Aniket Kadam <aniket.kadam@100ms.live>
2021-09-28 15:56:49 +0530

    Just some tests for utils.

---
+++ app/src/test/java/live/hms/app2/util/InternalUtilsKtTest.kt
@@ -0,0 +1,39 @@
+package live.hms.app2.util
+
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Test
+
+class InternalUtilsKtTest() {
+
+    private val meetingUrl = "https://aniket.app.100ms.live/meeting/correct-horse-battery"
+
+    @Test
+    fun testToSubdomain() {
+        val subdomain = meetingUrl.toSubdomain()
+
+//        URI(meetingUrl).host
+        assertThat(subdomain, equalTo("aniket.app.100ms.live"))
+    }
+
+    @Test
+    fun environment() {
+        val env = meetingUrl.getTokenEndpointEnvironment()
+        assertThat(env, equalTo("prod-in"))
+    }
+
+    @Test
+    fun `get meeting url code`() {
+        val groups = REGEX_MEETING_URL_CODE.findAll(meetingUrl).toList()[0].groupValues
+        val code = groups[2]
+
+//        val code = android.net.Uri.parse(meetingUrl).lastPathSegment
+        assertThat(code, equalTo("correct-horse-battery"))
+    }
+
+    @Test
+    fun `get token endpoint for join`() {
+        val tokenEndpoint = getTokenEndpointForCode(meetingUrl.getTokenEndpointEnvironment())
+        assertThat(tokenEndpoint, equalTo("https://prod-in.100ms.live/hmsapi/get-token"))
+    }
+}
