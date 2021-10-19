595dca36abb18db7695ff4fedbd4d2086c41f588
Aniket Kadam <aniket.kadam@100ms.live>
2021-10-19 16:21:31 +0530

    Updated with frame width/height and RTT

--- app/src/main/java/live/hms/app2/ui/meeting/pinnedvideo/StatsInterpreter.kt
+++ app/src/main/java/live/hms/app2/ui/meeting/pinnedvideo/StatsInterpreter.kt
@@ -43,8 +43,8 @@ class StatsInterpreter(val active: Boolean) : Closeable {
                         it.fold("") { acc, webrtcStats ->
                             acc + when (webrtcStats) {
                                 is Audio -> "\nAudio:\n\tJitter:${webrtcStats.jitter}\nPL:${webrtcStats.packetsLost}\nConcealment Events:${webrtcStats.concealmentEvents}"
-                                is Video -> "\nVideo:\n\tJitter:${webrtcStats.jitter}\nPL:${webrtcStats.packetsLost}\nFPS:${webrtcStats.framesPerSecond}\nFD:${webrtcStats.framesDropped}\nPLI:${webrtcStats.pliCount}\nNACK:${webrtcStats.nackCount}"
-                                is Peer -> "\nPeer:\n\tIncoming: ${webrtcStats.availableIncomingBitrate}\nOutgoing: ${webrtcStats.availableOutgoingBitrate}\n${webrtcStats.currentRoundTripTime}"
+                                is Video -> "\nVideo:\n\tJitter:${webrtcStats.jitter}\nPL:${webrtcStats.packetsLost}\nFPS:${webrtcStats.framesPerSecond}\nFD:${webrtcStats.framesDropped}\nPLI:${webrtcStats.pliCount}\nNACK:${webrtcStats.nackCount}\nWidth:${webrtcStats.frameWidth}\nHeight:${webrtcStats.frameHeight}"
+                                is Peer -> "\nPeer:\n\tIncoming: ${webrtcStats.availableIncomingBitrate}\nOutgoing: ${webrtcStats.availableOutgoingBitrate}\nRTT${webrtcStats.currentRoundTripTime}"
                                 else -> acc
                             }
                         }
