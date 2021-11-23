af75e0e35a60d09c16e9ad1d312d587861c261ad
Ashish1793 <ashish@100ms.live>
2021-11-23 17:54:15 +0530

    fix: changing fps to 30

--- app/src/main/java/live/hms/app2/ui/meeting/MeetingViewModel.kt
+++ app/src/main/java/live/hms/app2/ui/meeting/MeetingViewModel.kt
@@ -843,7 +843,7 @@ class MeetingViewModel(
     val imageBitmap = getBitmapFromAsset(context!!.applicationContext,"2.jpeg")
     plugin.setBackground(imageBitmap)
 //    add Plugin from sdk,
-    hmsSDK.addPlugin(plugin, 15)
+    hmsSDK.addPlugin(plugin, 30)
   }
 
   fun stopPlugin() {
