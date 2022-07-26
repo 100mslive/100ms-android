b55068e3280eb30f710b2c46893029e75ffc60be
Aniket Kadam <aniket.kadam@100ms.live>
2022-07-26 11:27:52 +0530

    Added getters for individual streaming permissions
    Signed-off-by: Aniket Kadam <aniket.kadam@100ms.live>

--- app/src/main/java/live/hms/app2/ui/meeting/MeetingViewModel.kt
+++ app/src/main/java/live/hms/app2/ui/meeting/MeetingViewModel.kt
@@ -883,6 +883,14 @@ class MeetingViewModel(
     return hmsSDK.getLocalPeer()?.hmsRole?.permission?.unmute == true
   }
 
+  fun isAllowedToRtmpStream() : Boolean = hmsSDK.getLocalPeer()?.hmsRole?.permission?.streaming == true ||
+          hmsSDK.getLocalPeer()?.hmsRole?.permission?.rtmpStreaming == true
+
+  fun isAllowedToBrowserRecord() : Boolean = hmsSDK.getLocalPeer()?.hmsRole?.permission?.browserRecording == true
+
+  fun isAllowedToHlsStream() : Boolean = hmsSDK.getLocalPeer()?.hmsRole?.permission?.streaming == true ||
+    hmsSDK.getLocalPeer()?.hmsRole?.permission?.hlsStreaming == true
+
   fun changeRole(remotePeerId: String, toRoleName: String, force: Boolean) {
     val hmsPeer = hmsSDK.getPeers().find { it.peerID == remotePeerId }
     val toRole = hmsSDK.getRoles().find { it.name == toRoleName }
