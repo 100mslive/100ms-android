0f72359a5f9ee21589c9d5dde9dc644e7864405f
pratim <pratim@100ms.live>
2022-09-01 18:46:32 +0530

    Adding logs for loggin exoplayer errors

--- app/src/main/java/live/hms/app2/ui/meeting/activespeaker/HlsFragment.kt
+++ app/src/main/java/live/hms/app2/ui/meeting/activespeaker/HlsFragment.kt
@@ -7,13 +7,19 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.navArgs
+import com.google.android.exoplayer2.ExoPlaybackException
+import com.google.android.exoplayer2.PlaybackException
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.source.BehindLiveWindowException
 import live.hms.app2.databinding.HlsFragmentLayoutBinding
 import live.hms.app2.ui.meeting.HlsPlayer
 import live.hms.app2.ui.meeting.MeetingViewModel
 import live.hms.app2.util.viewLifecycle
+import live.hms.video.utils.HMSLogger
 
 class HlsFragment : Fragment() {
 
+    private val TAG: String = "HlsFragment"
     private val args: HlsFragmentArgs by navArgs()
     private val meetingViewModel: MeetingViewModel by activityViewModels()
 
@@ -37,9 +43,37 @@ class HlsFragment : Fragment() {
 
     override fun onStart() {
         super.onStart()
-        binding.hlsView.player = hlsPlayer.getPlayer(requireContext(),
-                args.hlsStreamUrl,
-                true)
+        val exoPlayer = hlsPlayer.getPlayer(requireContext(),
+        args.hlsStreamUrl,
+        true)
+        binding.hlsView.player = exoPlayer
+
+        exoPlayer.addListener(object : Player.Listener{
+            override fun onPlayerError(error: PlaybackException) {
+                super.onPlayerError(error)
+                HMSLogger.i(TAG, " ~~ Exoplayer error: $error")
+            }
+
+            override fun onPlaybackStateChanged(playbackState: Int) {
+                super.onPlaybackStateChanged(playbackState)
+                HMSLogger.i(TAG, "Playback state change to $playbackState")
+            }
+        })
+
+    }
+
+    private fun isBehindLiveWindow(e: ExoPlaybackException): Boolean {
+        if (e.type != ExoPlaybackException.TYPE_SOURCE) {
+            return false
+        }
+        var cause: Throwable? = e.sourceException
+        while (cause != null) {
+            if (cause is BehindLiveWindowException) {
+                return true
+            }
+            cause = cause.cause
+        }
+        return false
     }
 
     override fun onStop() {
