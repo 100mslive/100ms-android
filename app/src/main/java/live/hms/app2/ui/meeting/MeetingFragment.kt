5c80001aeec1f12269ec9f785f97164f5c3afd2a
Aniket Kadam <aniket.kadam@100ms.live>
2022-09-23 16:20:53 +0530

    cleanup
    Signed-off-by: Aniket Kadam <aniket.kadam@100ms.live>

--- app/src/main/java/live/hms/app2/ui/meeting/MeetingFragment.kt
+++ app/src/main/java/live/hms/app2/ui/meeting/MeetingFragment.kt
@@ -1018,15 +1018,14 @@ class MeetingFragment : Fragment() {
                 }
 
                 is MeetingState.ForceLeave -> {
-                    with(state.details) {
-                        val message = if(roomWasEnded) {
+                    val message = with(state.details) {
+                        if(roomWasEnded) {
                             "Room ended by ${peerWhoRemoved?.name}"
                         } else {
                             "${peerWhoRemoved?.name} removed you from the room. ${state.details.reason}"
                         }
-
-                        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
                     }
+                    Toast.makeText(context, message, Toast.LENGTH_LONG).show()
 
                     meetingViewModel.leaveMeeting(state.details)
                 }
