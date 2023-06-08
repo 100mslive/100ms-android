31273e9afd59ef105290c595451fe3c23358eb93
Aniket Kadam <aniket.kadam@100ms.live>
2023-06-08 12:22:43 +0530

    More polls ui
    Signed-off-by: Aniket Kadam <aniket.kadam@100ms.live>

---
+++ app/src/main/java/live/hms/app2/ui/polls/PollsCreationFragment.kt
@@ -0,0 +1,9 @@
+package live.hms.app2.ui.polls
+
+import androidx.fragment.app.Fragment
+import live.hms.app2.databinding.FragmentMeetingBinding
+import live.hms.app2.util.viewLifecycle
+
+class PollsCreationFragment : Fragment(){
+//    private var binding by viewLifecycle<PollFra
+}
