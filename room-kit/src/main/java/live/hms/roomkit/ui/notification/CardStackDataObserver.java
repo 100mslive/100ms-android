f79ae146931c5e0a59c2583f778a6f571400feb3
Gulzar <gulzarhome54@gmail.com>
2023-08-31 12:13:58 +0530

    added notifcation stack

---
+++ room-kit/src/main/java/live/hms/roomkit/ui/notification/CardStackDataObserver.java
@@ -0,0 +1,65 @@
+package live.hms.roomkit.ui.notification;
+
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CardStackDataObserver extends RecyclerView.AdapterDataObserver {
+
+    private final RecyclerView recyclerView;
+
+    public CardStackDataObserver(RecyclerView recyclerView) {
+        this.recyclerView = recyclerView;
+    }
+
+    @Override
+    public void onChanged() {
+        CardStackLayoutManager manager = getCardStackLayoutManager();
+        manager.setTopPosition(0);
+    }
+
+    @Override
+    public void onItemRangeChanged(int positionStart, int itemCount) {
+        // Do nothing
+    }
+
+    @Override
+    public void onItemRangeChanged(int positionStart, int itemCount, @Nullable Object payload) {
+        // Do nothing
+    }
+
+    @Override
+    public void onItemRangeInserted(int positionStart, int itemCount) {
+        // Do nothing
+    }
+
+    @Override
+    public void onItemRangeRemoved(int positionStart, int itemCount) {
+        // 要素が削除された場合はTopPositionの調整が必要になる場合がある
+        // 具体的には、要素が全て削除された場合と、TopPositionより前の要素が削除された場合は調整が必要
+        CardStackLayoutManager manager = getCardStackLayoutManager();
+        int topPosition = manager.getTopPosition();
+        if (manager.getItemCount() == 0) {
+            // 要素が全て削除された場合
+            manager.setTopPosition(0);
+        } else if (positionStart < topPosition) {
+            // TopPositionよりも前の要素が削除された場合
+            int diff = topPosition - positionStart;
+            manager.setTopPosition(Math.min(topPosition - diff, manager.getItemCount() - 1));
+        }
+    }
+
+    @Override
+    public void onItemRangeMoved(int fromPosition, int toPosition, int itemCount) {
+        CardStackLayoutManager manager = getCardStackLayoutManager();
+        manager.removeAllViews();
+    }
+
+    private CardStackLayoutManager getCardStackLayoutManager() {
+        RecyclerView.LayoutManager manager = recyclerView.getLayoutManager();
+        if (manager instanceof CardStackLayoutManager) {
+            return (CardStackLayoutManager) manager;
+        }
+        throw new IllegalStateException("CardStackView must be set CardStackLayoutManager.");
+    }
+
+}
---
+++ room-kit/src/main/java/live/hms/roomkit/ui/notification/CardStackListener.java
@@ -0,0 +1,27 @@
+package live.hms.roomkit.ui.notification;
+
+import android.view.View;
+
+public interface CardStackListener {
+    void onCardDragging(Direction direction, float ratio);
+    void onCardSwiped(Direction direction);
+    void onCardRewound();
+    void onCardCanceled();
+    void onCardAppeared(View view, int position);
+    void onCardDisappeared(View view, int position);
+
+    CardStackListener DEFAULT = new CardStackListener() {
+        @Override
+        public void onCardDragging(Direction direction, float ratio) {}
+        @Override
+        public void onCardSwiped(Direction direction) {}
+        @Override
+        public void onCardRewound() {}
+        @Override
+        public void onCardCanceled() {}
+        @Override
+        public void onCardAppeared(View view, int position) {}
+        @Override
+        public void onCardDisappeared(View view, int position) {}
+    };
+}
