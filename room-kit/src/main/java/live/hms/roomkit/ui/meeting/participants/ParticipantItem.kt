17e4880d6ea47d257530fb42c8cbdc9dbfd75626
Aniket Kadam <aniket.kadam@100ms.live>
2023-09-04 17:14:21 +0530

    Allow force changing the role
    Signed-off-by: Aniket Kadam <aniket.kadam@100ms.live>

--- room-kit/src/main/java/live/hms/roomkit/ui/meeting/participants/ParticipantItem.kt
+++ room-kit/src/main/java/live/hms/roomkit/ui/meeting/participants/ParticipantItem.kt
@@ -21,7 +21,7 @@ import live.hms.video.sdk.models.HMSRemotePeer
 class ParticipantItem(private val hmsPeer: HMSPeer,
                       private val viewerPeer : HMSLocalPeer,
                       private val toggleTrack: (hmsPeer: HMSRemotePeer, type: HMSTrackType) -> Unit,
-                      private val changeRole: (remotePeerId: String, roleToChangeTo : String) -> Unit,
+                      private val changeRole: (remotePeerId: String, roleToChangeTo : String, force : Boolean) -> Unit,
                       private val isAllowedToChangeRole : Boolean,
                       private val isAllowedToMutePeers : Boolean,
                       private val isAllowedToRemovePeers : Boolean,
@@ -55,7 +55,7 @@ class ParticipantItem(private val hmsPeer: HMSPeer,
                                 override fun onSuccess() {
                                     val role = prebuiltInfoContainer.onStageExp(viewerPeer.hmsRole.name)?.onStageRole
                                     if(role != null)
-                                        changeRole(hmsPeer.peerID, role)
+                                        changeRole(hmsPeer.peerID, role, false)
                                 }
                             })
 
@@ -65,7 +65,7 @@ class ParticipantItem(private val hmsPeer: HMSPeer,
                             val role = participantPreviousRoleChangeUseCase.getPreviousRole(hmsPeer)
                             Log.d("RolesChangingTo","$role")
                             if(role != null)
-                                changeRole(hmsPeer.peerID, role)
+                                changeRole(hmsPeer.peerID, role, true)
                             true
                         }
 
