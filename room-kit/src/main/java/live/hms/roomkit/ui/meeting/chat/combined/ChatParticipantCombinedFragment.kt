039c6b150a5756bf740b7493e64477a1ab0dd121
Aniket Kadam <aniket.kadam@100ms.live>
2023-09-06 17:08:28 +0530

    Fix opening preview
    Signed-off-by: Aniket Kadam <aniket.kadam@100ms.live>

--- room-kit/src/main/java/live/hms/roomkit/ui/meeting/chat/combined/ChatParticipantCombinedFragment.kt
+++ room-kit/src/main/java/live/hms/roomkit/ui/meeting/chat/combined/ChatParticipantCombinedFragment.kt
@@ -90,7 +90,7 @@ companion object {
         }
 
         binding.closeCombinedTabButton.setOnSingleClickListener {
-            findNavController().popBackStack()
+            dismiss()
         }
 
         if(getShowParticipants())
@@ -117,7 +117,7 @@ companion object {
             viewLifecycleOwner,
             object : OnBackPressedCallback(true) {
                 override fun handleOnBackPressed() {
-                    findNavController().popBackStack()
+                    dismiss()
                 }
             })
     }
