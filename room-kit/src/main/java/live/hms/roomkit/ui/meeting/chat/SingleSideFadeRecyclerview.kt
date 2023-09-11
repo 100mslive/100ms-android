57c076045787368a56005db4fbeec74dbe0c9b74
Aniket Kadam <aniket.kadam@100ms.live>
2023-09-11 15:00:11 +0530

    Chat message spacing
    Signed-off-by: Aniket Kadam <aniket.kadam@100ms.live>

--- room-kit/src/main/java/live/hms/roomkit/ui/meeting/chat/SingleSideFadeRecyclerview.kt
+++ room-kit/src/main/java/live/hms/roomkit/ui/meeting/chat/SingleSideFadeRecyclerview.kt
@@ -2,8 +2,11 @@ package live.hms.roomkit.ui.meeting.chat
 
 import android.content.Context
 import android.util.AttributeSet
+import androidx.core.content.res.ResourcesCompat
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import live.hms.roomkit.R
 
 class SingleSideFadeRecyclerview  @JvmOverloads constructor(
     context: Context,
@@ -25,5 +28,9 @@ class SingleSideFadeRecyclerview  @JvmOverloads constructor(
             }
         isVerticalFadingEdgeEnabled = true
         setFadingEdgeLength(140)
+        val divider = DividerItemDecoration(context, VERTICAL).apply {
+            setDrawable(ResourcesCompat.getDrawable(resources, R.drawable.chat_item_divider, null)!!)
+        }
+        addItemDecoration(divider)
     }
 }
