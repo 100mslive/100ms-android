4b493d529c10fa5d1be8fd037afc8a7857c9ee20
Aniket Kadam <aniket.kadam@100ms.live>
2023-09-14 12:37:01 +0530

    Fix chat conditions
    Signed-off-by: Aniket Kadam <aniket.kadam@100ms.live>

--- room-kit/src/main/java/live/hms/roomkit/ui/meeting/PrebuiltInfoContainer.kt
+++ room-kit/src/main/java/live/hms/roomkit/ui/meeting/PrebuiltInfoContainer.kt
@@ -8,31 +8,29 @@ class PrebuiltInfoContainer(private val hmssdk: HMSSDK) {
     private val roleMap : MutableMap<String, HMSRoomLayout.HMSRoomLayoutData> = mutableMapOf()
     private val localPeer by lazy { hmssdk.getLocalPeer()!! }
 
-    fun isChatEnabled(isHls : Boolean) : Boolean {
-        return if(isHls) {
+    fun isChatEnabled(): Boolean =
+        // how do we even know if it's in hls? What if they have both?
             roleMap[localPeer.hmsRole.name]?.screens?.conferencing?.hlsLiveStreaming
-                ?.elements?.chat != null
-        } else {
+                ?.elements?.chat != null ||
             roleMap[localPeer.hmsRole.name]?.screens?.conferencing
                 ?.default?.elements?.chat != null
-        }
-    }
-    fun chatInitialStateOpen(isHls: Boolean): Boolean {
-        val isChatInitialOpen = if (isHls) {
+
+    fun chatInitialStateOpen(): Boolean {
+        val isChatInitialOpen =
             roleMap[localPeer.hmsRole.name]?.screens?.conferencing?.hlsLiveStreaming?.elements?.chat?.initialState == "CHAT_STATE_OPEN"
-        } else {
+                    ||
             roleMap[localPeer.hmsRole.name]?.screens?.conferencing
                 ?.default?.elements?.chat?.initialState == "CHAT_STATE_OPEN"
-        }
+
         // Initial open is only valid for overlay chat
-        return isChatOverlay(isHls) && isChatInitialOpen
+        return isChatOverlay() && isChatInitialOpen
     }
-    fun isChatOverlay(isHls : Boolean) = if(isHls) {
+    fun isChatOverlay() =
         roleMap[localPeer.hmsRole.name]?.screens?.conferencing?.hlsLiveStreaming?.elements?.chat?.overlayView == true
-    } else {
+                ||
         roleMap[localPeer.hmsRole.name]?.screens?.conferencing
             ?.default?.elements?.chat?.overlayView == true
-    }
+
 
     fun onStageExp(role : String) =
         roleMap[role]?.screens?.conferencing?.default?.elements?.onStageExp
