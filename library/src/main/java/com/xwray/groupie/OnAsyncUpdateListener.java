5ad27486734eadbfb8e47d9c3591ccf06b38879f
Aniket Kadam <aniket.kadam@100ms.live>
2023-10-13 12:07:46 +0530

    Remove groupie

--- library/src/main/java/com/xwray/groupie/OnAsyncUpdateListener.java
+++
@@ -1,9 +0,0 @@
-package com.xwray.groupie;
-
-/**
- * Listener that provides a callback when
- * async update methods complete.
- */
-public interface OnAsyncUpdateListener {
-    void onUpdateComplete();
-}
