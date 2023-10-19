abcf36399636da4afa7a766b197019657a3e1bb1
Aniket Kadam <aniket.kadam@100ms.live>
2023-10-19 14:32:52 +0530

    Formatting changes
    Signed-off-by: Aniket Kadam <aniket.kadam@100ms.live>

--- room-kit/src/main/java/live/hms/roomkit/ui/polls/display/voting/ProgressDisplayViewHolder.kt
+++ room-kit/src/main/java/live/hms/roomkit/ui/polls/display/voting/ProgressDisplayViewHolder.kt
@@ -8,14 +8,11 @@ import androidx.viewbinding.ViewBinding
 import live.hms.roomkit.R
 import live.hms.roomkit.databinding.LayoutPollsDisplayResultProgressBarsItemBinding
 import live.hms.roomkit.databinding.LayoutPollsDisplayResultQuizAnswerItemsBinding
-import live.hms.roomkit.drawableLeft
 import live.hms.roomkit.drawableStart
 import live.hms.roomkit.ui.theme.HMSPrebuiltTheme
 import live.hms.roomkit.ui.theme.applyTheme
 import live.hms.roomkit.ui.theme.getColorOrDefault
-import live.hms.video.polls.models.HmsPoll
 import live.hms.video.polls.models.HmsPollCategory
-import live.hms.video.polls.models.PollStatsQuestions
 import live.hms.video.polls.models.question.HMSPollQuestionType
 
 class ProgressDisplayViewHolder(
@@ -23,14 +20,17 @@ class ProgressDisplayViewHolder(
 ) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(item: ProgressBarInfo) {
-        if(item.category == HmsPollCategory.POLL) {
+        if (item.category == HmsPollCategory.POLL) {
             pollsBinding(binding as LayoutPollsDisplayResultProgressBarsItemBinding, item)
         } else {
             quizBinding(binding as LayoutPollsDisplayResultQuizAnswerItemsBinding, item)
         }
     }
 
-    private fun quizBinding(binding :LayoutPollsDisplayResultQuizAnswerItemsBinding, item : ProgressBarInfo) =with(binding){
+    private fun quizBinding(
+        binding: LayoutPollsDisplayResultQuizAnswerItemsBinding,
+        item: ProgressBarInfo
+    ) = with(binding) {
         applyTheme()
         optionText.text = item.optionText
         val numVotes = item.numberOfVotes.toInt()
@@ -38,33 +38,46 @@ class ProgressDisplayViewHolder(
             HMSPollQuestionType.singleChoice -> {
                 item.pollQuestionAnswer?.option == item.index + 1
             }
+
             HMSPollQuestionType.multiChoice -> {
                 item.pollQuestionAnswer?.options?.contains(item.index + 1) == true
             }
+
             else -> false
         }
-        if(isAnswerCorrect)
-            optionText.drawableStart = ResourcesCompat.getDrawable(root.resources, R.drawable.circle_tick, null)?.apply { setTint(
-                getColorOrDefault(
-                    HMSPrebuiltTheme.getColours()?.onSurfaceHigh,
-                    HMSPrebuiltTheme.getDefaults().onsurface_high_emp
-                )
-            )
-            }
+        if (isAnswerCorrect)
+            optionText.drawableStart =
+                ResourcesCompat.getDrawable(root.resources, R.drawable.circle_tick, null)?.apply {
+                    setTint(
+                        getColorOrDefault(
+                            HMSPrebuiltTheme.getColours()?.onSurfaceHigh,
+                            HMSPrebuiltTheme.getDefaults().onsurface_high_emp
+                        )
+                    )
+                }
 
-        peopleAnswering.text = binding.root.resources.getQuantityString(R.plurals.poll_quiz_results, numVotes, numVotes)
+        peopleAnswering.text = binding.root.resources.getQuantityString(
+            R.plurals.poll_quiz_results,
+            numVotes,
+            numVotes
+        )
     }
-    private fun pollsBinding(binding: LayoutPollsDisplayResultProgressBarsItemBinding, item : ProgressBarInfo) {
+
+    private fun pollsBinding(
+        binding: LayoutPollsDisplayResultProgressBarsItemBinding,
+        item: ProgressBarInfo
+    ) {
         with(binding) {
             applyTheme()
-            if(item.totalVoteCount == 0) {
+            if (item.totalVoteCount == 0) {
                 questionProgressBar.visibility = View.GONE
             } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
                 questionProgressBar.setProgress(item.percentage, true)
             }
             answer.text = item.optionText
             val numVotes = item.numberOfVotes.toInt()
-            totalVotes.text = binding.root.resources.getQuantityString(R.plurals.poll_votes, numVotes, numVotes)
+            totalVotes.text =
+                binding.root.resources.getQuantityString(R.plurals.poll_votes, numVotes, numVotes)
         }
     }
 
