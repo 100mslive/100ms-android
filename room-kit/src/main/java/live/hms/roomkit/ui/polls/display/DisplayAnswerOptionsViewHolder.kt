e505afb7d95307673efb56a2b28ead6e6c8b6149
Aniket Kadam <aniket.kadam@100ms.live>
2023-10-20 14:54:59 +0530

    Add proper states for after answering but before ending
    Signed-off-by: Aniket Kadam <aniket.kadam@100ms.live>

--- room-kit/src/main/java/live/hms/roomkit/ui/polls/display/DisplayAnswerOptionsViewHolder.kt
+++ room-kit/src/main/java/live/hms/roomkit/ui/polls/display/DisplayAnswerOptionsViewHolder.kt
@@ -14,7 +14,7 @@ class DisplayAnswerOptionsViewHolder(
     setItemSelected: (selected: Int, noOthers: Boolean) -> Unit,
     val answerSelectionUpdated: () -> Unit,
     ) : ViewHolder(binding.root){
-
+    var ignoreCheckChanges = false
     init {
         binding.applyTheme()
         if(bindingAdapterPosition != RecyclerView.NO_POSITION) {
@@ -22,28 +22,34 @@ class DisplayAnswerOptionsViewHolder(
         }
         // Both set the same property, only one will be used.
         binding.radioButton.setOnCheckedChangeListener { _, isChecked ->
-            if(isChecked) {
-                setItemSelected(bindingAdapterPosition, true)
-                answerSelectionUpdated()
+            if(!ignoreCheckChanges) {
+                if (isChecked) {
+                    setItemSelected(bindingAdapterPosition, true)
+                    answerSelectionUpdated()
+                }
             }
         }
 
         binding.checkbox.setOnCheckedChangeListener { _, isChecked ->
-            getItem(bindingAdapterPosition).apply {
-                this.isChecked = isChecked
+            if(!ignoreCheckChanges) {
+                getItem(bindingAdapterPosition).apply {
+                    this.isChecked = isChecked
+                }
+                answerSelectionUpdated()
             }
-            answerSelectionUpdated()
         }
     }
 
     fun bind(option : Option){
         with(binding) {
+            ignoreCheckChanges = true
             radioButton.isChecked = option.isChecked
             checkbox.isChecked = option.isChecked
+            ignoreCheckChanges = false
             checkbox.visibility = if(option.showCheckbox) View.VISIBLE else View.GONE
             radioButton.visibility = if(option.showCheckbox) View.GONE else View.VISIBLE
             text.setText(option.text)
-            if(option.hiddenAndAnswered && !canRoleViewVotes) {
+            if(option.hiddenAndAnswered) {
                 radioButton.isEnabled = false
                 checkbox.isEnabled = false
             }
