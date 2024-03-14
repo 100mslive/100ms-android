42d2b1b9ec183d1654c639933b396e616f25de4a
Aniket Kadam <aniket.kadam@100ms.live>
2024-03-14 16:37:54 +0530

    Hide the hand raise option when it shouldn't be there
    Signed-off-by: Aniket Kadam <aniket.kadam@100ms.live>

--- room-kit/src/main/java/live/hms/roomkit/ui/meeting/PrebuiltInfoContainer.kt
+++ room-kit/src/main/java/live/hms/roomkit/ui/meeting/PrebuiltInfoContainer.kt
@@ -152,6 +152,18 @@ class PrebuiltInfoContainer(private val hmssdk: HMSSDK) {
                 ?.hlsLiveStreamingHeader?.description
         }
     }
+
+    fun handRaiseAvailable() : Boolean {
+        val localPeer = hmssdk.getLocalPeer()
+        val available =  if (localPeer == null) {
+            hmsRoomLayout?.data?.get(0)?.screens?.conferencing?.hlsLiveStreaming?.elements
+                ?.handRaise != null
+        } else {
+            roleMap[localPeer.hmsRole.name]?.screens?.conferencing?.hlsLiveStreaming?.elements
+                ?.handRaise != null
+        }
+        return available
+    }
 }
 
 data class AllowedToMessageParticipants(
