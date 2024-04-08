4b810b7e03d328866aa2a3814c0c10dfcba3b574
Aniket Kadam <aniket.kadam@100ms.live>
2024-04-08 11:08:02 +0530

    Cleanup
    Signed-off-by: Aniket Kadam <aniket.kadam@100ms.live>

--- room-kit/src/main/java/live/hms/roomkit/ui/meeting/MeetingFragment.kt
+++ room-kit/src/main/java/live/hms/roomkit/ui/meeting/MeetingFragment.kt
@@ -26,7 +26,6 @@ import androidx.activity.OnBackPressedCallback
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
