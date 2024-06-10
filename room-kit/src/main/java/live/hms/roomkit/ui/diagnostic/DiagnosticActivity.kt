ae47d80931a0e83d77fce016d25295be7482bbf1
Gulzar <gulzarhome54@gmail.com>
2024-06-10 12:32:52 +0530

    base added

--- room-kit/src/main/java/live/hms/roomkit/ui/diagnostic/DiagnosticActivity.kt
+++ room-kit/src/main/java/live/hms/roomkit/ui/diagnostic/DiagnosticActivity.kt
@@ -2,9 +2,13 @@ package live.hms.roomkit.ui.diagnostic
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.activityViewModels
 import live.hms.roomkit.R
+import live.hms.roomkit.ui.meeting.MeetingViewModelFactory
 
 class DiagnosticActivity : AppCompatActivity() {
+
+    private val meetingViewModel: DiagnosticViewModelFactory = DiagnosticViewModelFactory(application)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_diagnostic)
--- room-kit/src/main/java/live/hms/roomkit/ui/diagnostic/DiagnosticViewModel.kt
+++ room-kit/src/main/java/live/hms/roomkit/ui/diagnostic/DiagnosticViewModel.kt
@@ -6,8 +6,14 @@ import live.hms.video.sdk.HMSSDK
 
 class DiagnosticViewModel(application: Application) : AndroidViewModel(application) {
     // First create a new sdk instance
-    val hmsSDK = HMSSDK.builder().build()
-    // Now get the diagonistic sdk instance
-// optional parameter to pass userId so we can send them the correct Data if req
-    val hmsDiagnostics = hmsSDK.getDiagnosticSDK(userId: String?)
+    val hmsSDK = HMSSDK.Builder(application).build()
+
+    fun setupDiagnosticSDK() {
+        // Now get the diagonistic sdk instance
+        // optional parameter to pass userId so we can send them the correct Data if req
+        val hmsDiagnostics = hmsSDK.getDiagnosticSDK("ssxs")
+
+    }
+
+
 }
