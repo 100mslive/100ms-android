1906569cb49edd70d3b0e7cf8382dcd6bebd60ea
Gulzar <gulzarhome54@gmail.com>
2024-06-10 13:52:27 +0530

    Update PreCallRegionSelectionFragment.kt

--- room-kit/src/main/java/live/hms/roomkit/ui/diagnostic/fragments/PreCallRegionSelectionFragment.kt
+++ room-kit/src/main/java/live/hms/roomkit/ui/diagnostic/fragments/PreCallRegionSelectionFragment.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import live.hms.roomkit.R
+import live.hms.roomkit.databinding.FragmentMeetingBinding
+import live.hms.roomkit.databinding.FragmentPreCallRegionSelectionBinding
+import live.hms.roomkit.setOnSingleClickListener
+import live.hms.roomkit.util.viewLifecycle
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,6 +27,8 @@ class PreCallRegionSelectionFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private var binding by viewLifecycle<FragmentPreCallRegionSelectionBinding>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -31,16 +38,24 @@ class PreCallRegionSelectionFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pre_call_region_selection, container, false)
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
 
+        binding = FragmentPreCallRegionSelectionBinding.inflate(inflater, container, false)
+        // Inflate the layout for this fragment
+        return binding.root
         //add code to create chip group and programmatically add chips
 
 
+    }
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.continueButton.setOnSingleClickListener {
+            //add code to navigate to next fragment
+            findNavController().navigate(PreCallRegionSelectionFragmentDirections.actionPreCallRegionSelectionFragmentToPreCallCameraFragment())
+        }
     }
 
     companion object {
@@ -54,12 +69,11 @@ class PreCallRegionSelectionFragment : Fragment() {
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PreCallRegionSelectionFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        fun newInstance(param1: String, param2: String) = PreCallRegionSelectionFragment().apply {
+            arguments = Bundle().apply {
+                putString(ARG_PARAM1, param1)
+                putString(ARG_PARAM2, param2)
             }
+        }
     }
 }
