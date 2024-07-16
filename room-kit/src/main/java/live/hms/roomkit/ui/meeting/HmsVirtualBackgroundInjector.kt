3af018d511d3d0cd13d53be90bdc0dcdf9e0f89d
Aniket Kadam <aniket.kadam@100ms.live>
2024-07-16 11:35:38 +0530

    Clarifying comments

--- room-kit/src/main/java/live/hms/roomkit/ui/meeting/HmsVirtualBackgroundInjector.kt
+++ room-kit/src/main/java/live/hms/roomkit/ui/meeting/HmsVirtualBackgroundInjector.kt
@@ -1,15 +1,15 @@
 package live.hms.roomkit.ui.meeting
 
-import android.graphics.Bitmap
 import live.hms.video.plugin.video.virtualbackground.HmsVirtualBackgroundInterface
-import live.hms.video.plugin.video.virtualbackground.VideoFrameInfoListener
 import live.hms.video.sdk.HMSSDK
 import live.hms.video.virtualbackground.HMSVirtualBackground
 
 class HmsVirtualBackgroundInjector(private val hmsSdk : HMSSDK) {
     val vbPlugin : HmsVirtualBackgroundInterface
     init {
-        val canLoadClass = try {
+        // implementation "live.100ms:virtual-background:$hmsVersion"
+        // without the above import being added, this will return false.
+        val isVbImportAdded = try {
             // the class is not local or an anonymous object so this can't be null
             // and if it was turned into a local one the crash would happen every time and
             // be noticeable
@@ -22,7 +22,7 @@ class HmsVirtualBackgroundInjector(private val hmsSdk : HMSSDK) {
             false
         }
 
-        vbPlugin = if(canLoadClass) HMSVirtualBackground(hmsSdk) else FakeVirtualBackground()
+        vbPlugin = if(isVbImportAdded) HMSVirtualBackground(hmsSdk) else FakeVirtualBackground()
 
     }
 }
