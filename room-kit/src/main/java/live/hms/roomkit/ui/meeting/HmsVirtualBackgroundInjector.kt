d389c06ba16de915d50b9336bdc0749cc1d8e9bd
Aniket Kadam <aniket.kadam@100ms.live>
2024-07-16 11:28:26 +0530

    avoid unnecessary warning

--- room-kit/src/main/java/live/hms/roomkit/ui/meeting/HmsVirtualBackgroundInjector.kt
+++ room-kit/src/main/java/live/hms/roomkit/ui/meeting/HmsVirtualBackgroundInjector.kt
@@ -10,7 +10,10 @@ class HmsVirtualBackgroundInjector(private val hmsSdk : HMSSDK) {
     val vbPlugin : HmsVirtualBackgroundInterface
     init {
         val canLoadClass = try {
-            val qualifiedName = HMSVirtualBackground::class.qualifiedName
+            // the class is not local or an anonymous object so this can't be null
+            // and if it was turned into a local one the crash would happen every time and
+            // be noticeable
+            val qualifiedName = HMSVirtualBackground::class.qualifiedName!!
             Class.forName(qualifiedName)
             true
         } catch (ex : ClassNotFoundException) {
