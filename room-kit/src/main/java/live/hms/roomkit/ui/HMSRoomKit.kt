32aa7d66b700a2920e43d86b5092f2b924c6fc95
Gulzar <gulzarhome54@gmail.com>
2024-09-10 11:27:34 +0400

    revert

--- room-kit/src/main/java/live/hms/roomkit/ui/HMSRoomKit.kt
+++ room-kit/src/main/java/live/hms/roomkit/ui/HMSRoomKit.kt
@@ -4,7 +4,6 @@ import android.app.Activity
 import android.content.Intent
 import androidx.core.content.ContextCompat.startActivity
 import live.hms.roomkit.ui.diagnostic.DiagnosticActivity
-import live.hms.roomkit.ui.meeting.LeakTestActivity
 import live.hms.roomkit.ui.meeting.MeetingActivity
 import live.hms.roomkit.util.ROOM_CODE
 import live.hms.roomkit.util.ROOM_PREBUILT
